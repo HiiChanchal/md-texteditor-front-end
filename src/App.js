41ebb0ab26abae73b16688cf4e34c3c5794fadc5
Chanchal Kumar Chandan <chanchal@pickright.in>
2024-09-20 13:50:11 +0530

    Added conainer and htmlpreview component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import MarkdownEditor from './components/MarkdownEditor';
+import HtmlPreview from './components/HtmlPreview';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>md-editor to process real-time preview</h1>
-    </div>
-  );
+    const [markdown, setMarkdown] = useState('');
+    const [html, setHtml] = useState('');
+
+    useEffect(() => {
+        const fetchHTML = async () => {
+            if (markdown) {
+                const response = await axios.post('/text/path/convert', { markdown });
+                setHtml(response.data.html);
+            } else {
+                setHtml('');
+            }
+        };
+
+        fetchHTML();
+    }, [markdown]);
+
+    return (
+        <div className="app-container">
+            <MarkdownEditor markdown={markdown} setMarkdown={setMarkdown} />
+            <HtmlPreview html={html} />
+        </div>
+    );
 }
 
 export default App;
