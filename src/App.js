7f6cabd238af36f07ab611121a3c648641549284
Chanchal Kumar Chandan <chanchal@pickright.in>
2024-09-20 14:07:40 +0530

    Added server port in env

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     useEffect(() => {
         const fetchHTML = async () => {
             if (markdown) {
-                const response = await axios.post('http://localhost:5001/text/path/convert', { markdown });
+                const response = await axios.post(`${process.env.REACT_APP_SERVER}/text/path/convert`, { markdown });
                 setHtml(response.data.html);
             } else {
                 setHtml('');
